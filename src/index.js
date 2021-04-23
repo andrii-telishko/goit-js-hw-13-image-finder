d73d978b3b3849410a55076b297f8e42be140297
Andrii Telishko <telishkoav@gmail.com>
2021-04-23 15:38:10 +0300

    add pagination

--- src/index.js
+++ src/index.js
@@ -7,22 +7,85 @@ const refs = {
     input: document.querySelector('[name="query"]'),
     submitBtn: document.querySelector('.upload-btn'),
     searchForm: document.querySelector('.search-form'),
-    renderImagesSection: document.querySelector('.gallery-section')
+    renderImagesSection: document.querySelector('.gallery-section'),
+    loadMoreBtn: document.querySelector('.load-more-btn'),
+    searchBtnSpinner: document.querySelector('.search-button-spinner'),
+    loadBtnSpinner: document.querySelector('.load-button-spinner'),
+    submitBtnText: document.querySelector('.submit-button-text'),
+    loadMoreBtnText: document.querySelector('.load-button-text'),
+    resetBtn: document.querySelector('.reset-button')
 }
+let pageNumber = 1;
+const KEY = '21283413-606cd1182a523c739b6934f12';
+// const searchQuery = refs.input.value;
 
 console.log(refs.input);
 
-refs.searchForm.addEventListener('submit', onSubmit)
+refs.submitBtn.addEventListener('click', onSubmit)
 
+
+    
 function onSubmit(event) {
     event.preventDefault();
-    const searchQuery = event.currentTarget.elements.query.value;
-    let pageNumber = 1;
-    const KEY = '21283413-606cd1182a523c739b6934f12';
+    const searchQuery = refs.input.value;
 
-    console.log(searchQuery);
-
+    console.log('submit');
+    
+    
+      
+     
+        refs.searchBtnSpinner.classList.remove('is-hidden')
+    pageNumber = 1;
+    refs.loadMoreBtn.classList.remove('is-hidden');
+    refs.submitBtn.setAttribute('disabled', true);
+    refs.submitBtn.classList.add('disabled')
+    refs.submitBtnText.textContent = 'Loading...'
     return fetch(`https://pixabay.com/api?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${KEY}`).then(response => response.json()).then(images => {
-        refs.renderImagesSection.innerHTML = galleryListTpl(images.hits)
+        
+        refs.submitBtnText.textContent = 'Upload images'
+        refs.renderImagesSection.innerHTML = galleryListTpl(images.hits);
+        pageNumber += 1;
+        refs.searchBtnSpinner.classList.add('is-hidden'); 
+    }
+       
+    );
+    
+    
+   
+    
+    
+}
+
+refs.loadMoreBtn.addEventListener('click', onLoadMore)
+
+function onLoadMore(event) {
+    // event.preventDefault();
+    console.log('load more');
+    const searchQuery = refs.input.value;
+    
+    
+    refs.loadBtnSpinner.classList.remove('is-hidden');
+    refs.loadMoreBtnText.textContent = 'Loading...';
+    console.log(searchQuery);
+    return fetch(`https://pixabay.com/api?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${pageNumber}&per_page=12&key=${KEY}`).then(response => response.json()).then(images => {
+        refs.loadBtnSpinner.classList.add('is-hidden');
+        refs.loadMoreBtnText.textContent = 'Load more';
+        refs.renderImagesSection.insertAdjacentHTML('beforeend', galleryListTpl(images.hits));
+        pageNumber += 1;
     });
+}
+
+
+refs.resetBtn.addEventListener('click', resetPage);
+
+function resetPage(e) {
+    e.preventDefault();
+    pageNumber = 1;
+        refs.submitBtn.setAttribute('disabled', false);
+        refs.renderImagesSection.innerHTML = '';
+        refs.submitBtn.classList.remove('disabled');
+    refs.loadMoreBtn.classList.add('is-hidden');
+    refs.renderImagesSection.innerHTML = '';
+    refs.input.value = '';
+    console.log('reset');
 }
